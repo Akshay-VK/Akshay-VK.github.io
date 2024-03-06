18400e7d426e18d3bb6bcb042a742ae1564a043e
Akshay VK <akshaykaruvathil@gmail.com>
2024-03-06 23:49:07 +0530

    Got something on the screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,71 @@
-<script>
-	import { fade } from 'svelte/transition';
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import anime from 'animejs';
+	import Canvas from './Canvas.svelte';
+
+	/**
+	 * @type {anime.AnimeInstance}
+	 */
+	let anim;
+
+	let a = 0;
+	onMount(() => {
+		a = 1;
+	});
+
+	function forward(
+		_node: HTMLDivElement,
+		{ targets, duration }: { targets: string; duration: number }
+	) {
+		var textWrapper: HTMLSpanElement = document.querySelector(
+			'.hero-name strong'
+		) as HTMLSpanElement;
+		textWrapper.innerHTML =
+			textWrapper.textContent?.replace(/\S/g, "<span class='letter'>$&</span>") || '';
+		console.log(textWrapper);
+		return {
+			css: (_t: any) => {
+				anime({
+					targets: '.hero-name strong .letter',
+					opacity: [0, 1],
+					easing: 'easeInOutQuad',
+					duration,
+					delay: (_el, i) => 30 * (i +1)
+				});
+				return '';
+			}
+		};
+	}
+	
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Karuvathil Akshay Vijaykumar</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-	<h1 class="font-serif">
-		<div>
-			<strong class="text-sky-50 text-6xl">Karuvathil Akshay Vijaykumar</strong>
-		</div>
-		<canvas id="can">Not supported.</canvas>
-	</h1>
+	{#if a == 1}
+		<h1 class="font-serif">
+			<div class="hero-name" in:forward={{ targets: '.hero-name', duration: 3000 }}>
+				<strong class="text-sky-50 text-6xl">Karuvathil Akshay Vijaykumar</strong>
+			</div>
+			<div class='can'>
+				<Canvas/>
+			</div>
+		</h1>
+	{/if}
 </section>
 
 <style lang="postcss">
+	.can {
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: -1;
+		width: 100vw;
+		height: 100vh;
+	}
 	section {
 		display: flex;
 		flex-direction: column;
@@ -28,14 +77,7 @@
 	h1 {
 		width: 100%;
 	}
-	canvas {
-		position: fixed;
-		top: 0;
-		left: 0;
-		z-index: -1;
-		width: 100vw;
-		height: 100vh;
-	}
+	
 	h1 {
 		animation: fadeInAnimation ease 1s;
 		animation-iteration-count: 1;
---
+++ src/routes/Canvas.svelte
@@ -0,0 +1,29 @@
+<script>
+	import P5  from 'p5-svelte';
+	
+  
+	const sketch = (/** @type {{ setup: () => void; createCanvas: (arg0: number, arg1: number) => void; draw: () => void; ellipse: (arg0: number, arg1: number, arg2: number, arg3: number) => void; width: number; height: number; }} */ p5) => {
+	  p5.setup = () => {
+		p5.createCanvas(p5.windowWidth, p5.windowHeight);
+	  };
+  
+	  p5.draw = () => {
+		p5.background(0,0,0,10);
+		p5.noStroke();
+		p5.fill(25)
+		let s=Math.abs(Math.sin(p5.frameCount/50))*200+100;
+		p5.ellipse(p5.width / 2, p5.height / 2-75, s, s);
+	  };
+	};
+  </script>
+  <P5 {sketch} />
+  <style>
+	canvas {
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: -1;
+		width: 100vw;
+		height: 100vh;
+	}
+  </style>
