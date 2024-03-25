492dfca282df7dc5479cc0d12f883014c585f91c
Akshay VK <akshaykaruvathil@gmail.com>
2024-03-25 08:12:36 +0530

    Adde webp version of images for safety

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import img from '$lib/images/picture-of-me.jpg';
+	import img from '$lib/images/picture-of-me.webp';
+	import img_alt from '$lib/images/picture-of-me.jpg';
 	const today = new Date();
 	const months = [
 		'January',
@@ -69,11 +70,10 @@
 		<div class="row-span-7 col-span-3 bg-amber-100 border border-black p-4">
 			<div>Middle Left</div>
 			<div>
-				<img
-				src={img}
-				alt="image goes here"
-				class="sepia hover:sepia-0 transition duration-500 w-2/3 contrast-75 hover:contrast-100 border border-black float-left p-1 m-2"
-				/>
+				<picture class="sepia hover:sepia-0 transition duration-500 w-2/3 contrast-75 hover:contrast-100 border border-black float-left p-1 m-2">
+					<source srcset={img} type="image/webp" />
+					<img src={img_alt} alt="Sup." />
+				</picture>
 			</div>
 			<div class="text-base body-style">
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac sapien ligula.
