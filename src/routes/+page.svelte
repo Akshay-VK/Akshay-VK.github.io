9f8f25d5984ba3543874faadb4bfc4cf4f12be52
Akshay VK <akshaykaruvathil@gmail.com>
2024-04-13 23:05:15 +0530

    Feat: Finalised the layout and implemented it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,7 @@
 		let elem = Array.from(document.querySelectorAll('.bg-amber-100'));
 		elem.shift();
 		elem.shift();
+		elem.pop();
 		console.log(elem);
 		let elementMap=new Map();
 		elem.forEach((element) => {
@@ -52,72 +53,79 @@
 
 <section class=" text-black grid w-full h-5/6 text-4xl overflow-x-auto">
 	<div
-		class="w-[60rem] aspect-[4/5] bg-amber-100 grid grid-rows-10 grid-cols-6 grid-flow-row gap-y-1 gap-x-1 justify-items-stretch  origin-top-left"
+		class="w-[60rem] aspect-[2/3] bg-amber-100 grid grid-rows-21 grid-cols-10 grid-flow-row gap-y-1 gap-x-1 justify-items-stretch  origin-top-left"
 	>
-		<div class="main-heading col-span-6 row-span-1 bg-amber-100 border border-black p-2 grid content-center">
+		<div class="main-heading col-span-10 row-span-1 bg-amber-100 border border-black p-2 grid content-center">
 			<Masthead/>
 		</div>
-		<div class="main-heading col-span-6 row-span-1 border p-2 grid content-center transition-transform duration-500">
+		<div class="main-heading col-span-10 row-span-1 border p-2 grid content-center transition-transform duration-500">
 			<Headline>FROM PIXELS TO PLANETS, HERE'S MY STORY</Headline>
 		</div>
 		<div
-			class="row-span-6 col-span-3 bg-amber-100 border border-black p-4 transition-transform duration-500 overflow-hidden mb-1 dir-right"
+			class="row-span-8 col-span-5 bg-amber-100 border border-black p-4 transition-transform duration-500 overflow-hidden mb-1 dir-right"
 		>
 			<!-- This is how the Section works....divs are used with the slot attribute to define
 			various parts of the main section like the headline, an image of it, the content the article,
 			and deck etc.  -->
 			<Section>
 				<div slot='headline'>So..who am I?</div>
+				<div slot="content">
+					Lorem ipsum dolor sit amet
+					<Link text="Continued on page 2 "/>
+				</div>
 				<div slot='deck'>Well, many things.</div>
 				<div slot='image'><Image src='{base}/picture-of-me.webp'/></div>
 			</Section>
 		</div>
 		<div
-			class="row-span-6 col-span-3 bg-amber-100 border border-black p-4 transition-transform duration-500  overflow-hidden mb-1 dir-left"
+			class="row-span-8 col-span-5 bg-amber-100 border border-black p-4 transition-transform duration-500  overflow-hidden mb-1 dir-left"
 		>
 			<Section>
 				<div slot="headline">In the spotlight</div>
 				<div slot="content">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac sapien ligula.
-					Pellentesque aliquet id orci vel aliquam. Fusce egestas tincidunt lacus, vel gravida enim
-					imperdiet in. Quisque eget rutrum eros. Nullam ac velit sapien. Nulla dignissim justo at
-					massa iaculis dignissim. Integer eleifend semper metus in viverra. Suspendisse tempor in
-					enim vitae finibus. Cras dapibus faucibus varius. In in viverra odio. Donec id tellus eget
-					mauris congue congue. Aliquam erat volutpat. Proin tempor massa vitae sapien maximus, at
-					sagittis nisi porta. Proin eu suscipit mi, ac fringilla metus. Fusce sed urna non felis
-					lobortis venenatis. Integer risus enim, aliquam nec erat vel, maximus auctor velit. Aliquam
-					tincidunt tellus nec tortor lobortis, non eleifend arcu pellentesque. Fusce varius justo at
-					congue convallis. Donec neque quam, placerat id condimentum ut, blandit eu nisi. Etiam
-					pharetra consequat est, ut condimentum neque pellentesque feugiat. Aliquam tellus turpis,
-					mollis vel facilisis vitae, imperdiet vel mauris. Proin sollicitudin purus eget leo
-					hendrerit, quis ultricies metus condimentum. Nunc gravida vitae quam sit amet molestie.
-					Proin eu sodales nunc. Curabitur vehicula facilisis gravida.
+					Lorem ipsum dolor sit amet
 					<Link text="Continued on page 2 "/>
 				</div>
 			</Section>
 		</div>
 		<div
-			class="row-span-2 col-span-2 bg-amber-100 border border-black p-4 transition-transform duration-500 overflow-hidden mb-1 dir-right"
+			class="row-span-10 col-span-2 bg-amber-100 border border-black p-4 transition-transform duration-500 overflow-hidden mb-1 dir-right"
 		>
 			<Section>
-				<div slot='headline'>Down Left</div>
+				<div slot='headline'>Featured projects</div>
 				<div slot='content'></div>
 			</Section>
 		</div>
 		<div
-			class="col-span-2 row-span-2 bg-amber-100 border border-black p-4 transition-transform duration-500 overflow-hidden mb-1"
+			class="col-span-4 row-span-10 bg-amber-100 border border-black p-4 transition-transform duration-500 overflow-hidden mb-1"
 		>
 			<Section>
-				<div slot='headline'>Down Middle</div>
+				<div slot='headline'>Timeline of success</div>
 				<div slot='content'></div>
 			</Section>
 		</div>
 		<div
-			class="col-span-2 row-span-2 bg-amber-100 border border-black p-4 transition-transform duration-500  overflow-hidden mb-1 dir-left"
+			class="col-span-4 row-span-5 bg-amber-100 border border-black p-4 transition-transform duration-500  overflow-hidden mb-1 dir-left"
 		>
 			<Section>
-				<div slot='headline'>Down Right</div>
+				<div slot='headline'>Skills and experience</div>
 				<div slot='content'>Yea. I must say, this works pretty well. Love it.</div>
+			</Section>
+		</div>
+		<div
+			class="col-span-4 row-span-5 bg-amber-100 border border-black p-4 transition-transform duration-500  overflow-hidden mb-1 dir-left"
+		>
+			<Section>
+				<div slot='headline'>Beyond academics</div>
+				<div slot='content'>Yea. I must say, this works pretty well. Love it.</div>
+			</Section>
+		</div>
+		<div
+			class="col-span-10 row-span-1 bg-amber-100 border border-black p-4 transition-transform duration-500  overflow-hidden mb-1 dir-right"
+		>
+			<Section>
+				<div slot='headline'>Contact</div>
+				<div slot='content'>Hey! You can reach me with my gmail id <span class="font-bold italic hover:underline">akshaykaruvathil@gmail.com</span></div>
 			</Section>
 		</div>
 	</div>
